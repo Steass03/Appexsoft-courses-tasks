1b0d96f7724b2033c0f14c50e80467d83a796bbf
Stas Senishch <senishchs@gmail.com>
2023-08-08 11:25:46 +0300

    lab5

---
+++ lesson 5/classwork/index.js
@@ -0,0 +1,51 @@
+// const a = "Hello BOB";
+// console.log(a);
+
+// let b = 2;
+// b = 4;
+// console.log(b);
+
+// console.log(a);
+// var a = 2;
+// var a = 3;
+
+// for (let index = 0; index < 1; index++) {
+//   const b = 2;
+// }
+// function a() {
+//   var c = true;
+// }
+// console.log(c);
+
+// function sum(a, b, c, d) {
+//   const result = a + b + c + d;
+//   return result;
+// }
+// const result = sum(1, 1, 2, 3);
+// console.log("This is result of sum: " + result);
+
+// const res = function sub(a, b) {
+//   return a - b;
+// };
+// console.log("This is result of sub: " + res(123, 5));
+// const multiply = (a, b) => {
+//   return a * b;
+// };
+// console.log("This is result of multiply: " + multiply(5, 8));
+// function divide(a, b) {
+//   return a / b;
+// }
+// console.log("This is result of divide: " + divide(100, 2));
+
+// let cycle = 1;
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log((cycle += i));
+// }
+// alert("Hello world");
+// setTimeout(() => {
+//   alert("You are good boy !!!");
+// }, 3000);
+
+// let result = prompt("Скільки вам років", 10);
+// console.log(`Ви запитали скільки вам років ${result} `);
